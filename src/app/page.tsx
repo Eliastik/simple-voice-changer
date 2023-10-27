2b9d9dd9b9047fe6bf7b6bcd13f2bbfa747a8dab
Eliastik <26941242+Eliastik@users.noreply.github.com>
2023-10-27 23:28:53 +0200

    Add confirmation message before exiting app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,22 @@
+"use client";
+
+import { useEffect } from "react";
 import MainComponent from "./components/MainComponent";
 
 const Home = () => {
+  useEffect(() => {
+    const handleBeforeUnload = (event: BeforeUnloadEvent) => {
+      event.preventDefault();
+      event.returnValue = '';
+    };
+
+    window.addEventListener("beforeunload", handleBeforeUnload);
+
+    return () => {
+      window.removeEventListener("beforeunload", handleBeforeUnload);
+    };
+  }, []);
+
   return <MainComponent></MainComponent>
 };
 
