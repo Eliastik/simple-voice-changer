344b5827d471dcd6644b580e8d0a54c3cbaa41cf
Eliastik <26941242+Eliastik@users.noreply.github.com>
2023-11-03 23:58:33 +0100

    Fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,14 @@
 
 import { useEffect } from "react";
 import MainComponent from "./components/MainComponent";
+import { useAudioEditor } from "./context/AudioEditorContext";
 
 const Home = () => {
+  const { pauseAudioBuffer } = useAudioEditor();
+  
   useEffect(() => {
     const handleBeforeUnload = (event: BeforeUnloadEvent) => {
+      pauseAudioBuffer();
       event.preventDefault();
       event.returnValue = '';
     };
