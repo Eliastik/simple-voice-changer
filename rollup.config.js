e9464b5a8459bea07f9a4824c5c334fe73821a2e
Eliastik <26941242+Eliastik@users.noreply.github.com>
2023-12-22 22:30:02 +0100

    Update dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import path from "path";
 import resolve from "@rollup/plugin-node-resolve";
 import cleanup from "rollup-plugin-cleanup";
 import typescript from "@rollup/plugin-typescript";
-import { terser } from "rollup-plugin-terser";
+import terser from "@rollup/plugin-terser";
 
 const config = [
     {
