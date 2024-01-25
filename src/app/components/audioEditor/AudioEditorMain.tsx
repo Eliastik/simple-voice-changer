dd6ed9e45b284c96cb0c08c7b83856c2fc062d9a
Eliastik <26941242+Eliastik@users.noreply.github.com>
2024-01-25 23:40:22 +0100

    Fix

--- src/app/components/audioEditor/AudioEditorMain.tsx
+++ src/app/components/audioEditor/AudioEditorMain.tsx
@@ -12,7 +12,7 @@ const AudioEditorMain = () => {
                 <div className="grid lg:grid-cols-6 md:grid-cols-4 grid-cols-3 md:gap-4 gap-2 place-content-center p-2 md:mt-2 md:p-0">
                     <FilterButtonList></FilterButtonList>
                 </div>
-                <div className="flex flex-row md:gap-x-3 gap-x-1 sticky bottom-20">
+                <div className="flex flex-row md:gap-x-3 gap-x-1 sticky bottom-20 max-w-full flex-wrap justify-center gap-y-1">
                     <AudioEditorActionButtons></AudioEditorActionButtons>
                 </div>
             </div>
