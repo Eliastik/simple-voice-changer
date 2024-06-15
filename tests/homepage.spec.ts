2f0cb73faca342dff2dd2689a6c1b62d175f7eb2
Eliastik <26941242+Eliastik@users.noreply.github.com>
2024-06-15 12:27:24 +0200

    Add test

--- tests/homepage.spec.ts
+++ tests/homepage.spec.ts
@@ -44,3 +44,34 @@ test("opening audio file should not display error", async ({ page }) => {
 
     expect(!(await modalErrorLoadingDialog.isVisible()));
 });
+
+
+test("returning to homepage should work", async ({ page }) => {
+    const openFileButton = page.locator("body > div:not(.navbar) > button");
+    const fileChooserPromise = page.waitForEvent("filechooser");
+
+    openFileButton.click();
+
+    const fileChooser = await fileChooserPromise;
+    await fileChooser.setFiles(path.join(__dirname, "files/audio.mp3"));
+
+    const homepageButton = page.locator(".navbar > .btn.btn-ghost:nth-child(1)");
+
+    await homepageButton.waitFor({ state: "visible", timeout: 5000 });
+
+    expect(!(await openFileButton.isVisible()));
+
+    await homepageButton.click();
+
+    const modalGoToHome = page.locator("#modalGoToHome");
+    
+    await modalGoToHome.waitFor({ state: "attached", timeout: 5000 });
+
+    const returnToHomeModalButton = page.locator("#modalGoToHome .btn", { hasText: "OK" });
+
+    await returnToHomeModalButton.waitFor({ state: "visible", timeout: 500 });
+
+    await returnToHomeModalButton.click();
+
+    expect(await openFileButton.isVisible());
+});
